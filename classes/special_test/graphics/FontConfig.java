e68556b620e56f0b3f34581aeae4b5ea935eba2b
Giles Lavelle <giles.lavelle@gmail.com>
2013-06-22 19:21:43 +0100

    Added class to test font configuration from the command line -- directly instantiating Swing components throws a HeadlessException

---
+++ classes/special_test/graphics/FontConfig.java
@@ -0,0 +1,13 @@
+package classes.special_test.graphics;
+
+import sun.awt.FontConfiguration;
+import classes.awt.CanvasGraphicsEnvironment;
+
+public class FontConfig {
+    public static void main(String[] args){
+        CanvasGraphicsEnvironment ge =
+            (CanvasGraphicsEnvironment) CanvasGraphicsEnvironment.getLocalGraphicsEnvironment();
+
+        FontConfiguration fc = ge.createFontConfiguration(true, true);
+    }
+}
